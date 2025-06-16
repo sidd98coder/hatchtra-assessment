33367d3eed249985de4bf26b6251484d6d95cf16
siddantvardey <siddant.vardey@spacejoy.com>
2025-06-16 15:08:14 +0530

    Initial commit with React + Lucide setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import IconCard from './components/IconCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Working demo of the file explorer</h1>
+      <IconCard />
+    </div>
+  );
+}
+
+export default App;
